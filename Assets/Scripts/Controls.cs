504bccdbd978c51b4feb1472caa758a1979efef8
YevheniiKalyta <56540544+YevheniiKalyta@users.noreply.github.com>
2020-08-02 17:11:14 +0200

    Added files

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,64 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 2f, gravity = -9.8f, groundDistance = 5f, jumpHeight = 2f,floorHeight=6f;
+    public int playerCurFloor;
+
+    Vector3 velocity;
+
+    public Transform feet;
+    public LayerMask groundLayer;
+    bool isGrounded;
+    public GameObject elevator;
+    Rigidbody rb;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(feet.position, groundDistance, groundLayer);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+
+
+        rb.velocity = new Vector3(move.x * speed * Time.deltaTime, rb.velocity.y,move.z * speed * Time.deltaTime) ;
+       
+
+        
+        
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpHeight);
+        }
+
+
+
+        PlayerCurrFloorMath();
+
+
+    }
+
+    public void PlayerCurrFloorMath()
+    {
+        if (transform.position.y % floorHeight -0.89 < 0.1f )
+        {
+            playerCurFloor = (int)(transform.position.y / floorHeight);
+        }
+    }
+}
---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour
+{
+    public LayerMask buttonMask;
+    public Image crosshair;
+    public Sprite interactionSprite,idleSprite;
+    public Text text;
+    public float t, instructionsTime;
+    bool instructions = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward,out hit,5,buttonMask))
+        {
+            if (crosshair.sprite == idleSprite)
+            {
+                if (instructions)
+                {
+                    text.enabled = true;
+                }
+                crosshair.sprite = interactionSprite;
+                crosshair.gameObject.GetComponent<RectTransform>().localScale= Vector3.one * 5;
+                
+            }
+
+            //Pressing the button
+            if (Input.GetButtonDown("Fire1"))
+            {
+                hit.transform.gameObject.GetComponent<ButtonScript>().ButtonClick();
+            }
+
+        }
+        else if(crosshair.sprite == interactionSprite)
+        {
+            crosshair.sprite = idleSprite;
+            text.enabled = false;
+            crosshair.gameObject.GetComponent<RectTransform>().localScale = Vector3.one ;
+        }
+
+        if (text.enabled == true)
+        {
+            t += Time.deltaTime;
+            if (t >= instructionsTime)
+            {
+                instructions = false;
+                text.enabled = false;
+            }
+        }
+
+        
+    }
+}
